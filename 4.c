0315de04c2f3f7f1ca0e286836ba339ba12a1f5f
AlomgirH <102249051+Alomgir01@users.noreply.github.com>
2022-12-24 14:11:16 +0600

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+    int i,j, value ;
+    float x;
+    printf("Enter the number: ");
+    scanf("%d%f%d\n",i,x,j);
+
+    value=(i+x)*j;
+    printf("Shows the number: ",value);
+
+
+}
